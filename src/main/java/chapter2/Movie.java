6991bdb899cd7df4ef1fbb7523dd352410978eeb
cwJohnPark <pierrotcw@gmail.com>
2020-06-04 06:38:29 +0900

    Add chapter2 Movie Reservation System

---
+++ src/main/java/chapter2/Movie.java
@@ -0,0 +1,12 @@
+package chapter2;
+
+
+/**
+ * 영화
+ */
+public class Movie {
+
+    public Money calculateMovieFee(Screening screening) {
+        return null;
+    }
+}
---
+++ src/main/java/chapter2/Screening.java
@@ -0,0 +1,38 @@
+package chapter2;
+
+import java.time.LocalDateTime;
+
+/**
+ * 영화를 예매하는 사건을 형상화
+ */
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreend;
+
+    public Screening(Movie movie, int sequence, LocalDateTime whenScreend) {
+        this.movie = movie;
+        this.sequence = sequence;
+        this.whenScreend = whenScreend;
+    }
+
+    public Reservation reserve(Customer customer, int audienceCount) {
+        return new Reservation(customer, this, calculateFee(audienceCount), audienceCount);
+    }
+
+    private Money calculateFee(int audienceCount) {
+        return movie.calculateMovieFee(this).times(audienceCount);
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public boolean isSequence(int sequence) {
+        return this.sequence == sequence;
+    }
+
+    public LocalDateTime getStartTime() {
+        return whenScreend;
+    }
+}
